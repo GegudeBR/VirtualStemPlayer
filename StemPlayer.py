1db11ed70fb408a4260b95a7a32c8739dc38e30b
George <67044516+GegudeBR@users.noreply.github.com>
2022-03-14 16:15:28 -0700

    Added Select Song
    + Selected song functionalities
    - pretrained_modules/ added to .gitignore

--- StemPlayer.py
+++ StemPlayer.py
@@ -1,6 +1,8 @@
 import os
 import vlc
 from tkinter import *
+from tkinter.messagebox import showinfo
+from tkinter import filedialog as fd
 import tempfile
 from spleeter.separator import Separator
 
@@ -11,12 +13,17 @@ class App:
     self.is_playing = False
     self.song_loaded = False
     self.vocals_mp3, self.drums_mp3, self.bass_mp3, self.other_mp3 = None, None, None, None
+    self.song_path = None
+    self.tempdir = tempfile.mkdtemp(prefix="virtual_stem_player-")
+
     self.root = Tk()
-    self.pause = Button(self.root, command=self.pause_and_play)
+    self.pause = Button(self.root, command=self.pause_and_play, state=DISABLED)
     self.vocals = Scale(self.root, from_=100, to=0, command=self.update_vocals)
     self.drums = Scale(self.root, from_=100, to=0, orient=HORIZONTAL, command=self.update_drums)
     self.bass = Scale(self.root, from_=0, to=100, orient=HORIZONTAL, command=self.update_bass)
     self.other = Scale(self.root, from_=0, to=100, command=self.update_other)
+
+    self.song_button = Button(self.root, text="Select Song",  command=self.select_song)
 
     allStems = [self.vocals, self.drums, self.bass, self.other]
 
@@ -35,11 +42,11 @@ class App:
     self.other.place(x=center_x,y=180)
 
     self.pause.place(x=center_x + 25,y=130,height=30, width=30)
+    self.song_button.place(x=10, y=10)
 
     self.root.title("Virtual Stem Player")
     self.root.geometry("300x300")
     self.root.resizable(False, False)
-    self.setup()
     self.root.mainloop()
 
   def update_vocals(self, event):
@@ -56,22 +63,52 @@ class App:
       self.other_mp3.audio_set_volume(self.other.get()) 
 
   def setup(self):
-    #tempdir = tempfile.mkdtemp(prefix="virtual_stem_player-")
-    #cut = Separator('spleeter:4stems')
-    tempdir = "/Users/hallaig/Desktop"
-    input_path = "/Users/hallaig/Desktop/Happier_Than_ever.mp3"
-    split_path = input_path.split("/")
+    
+    cut = Separator('spleeter:4stems')
+    split_path = self.song_path.split("/")
     song_name = split_path[len(split_path)-1].replace(".mp3", "")
-    #cut.separate_to_file(input_path, tempdir);
-    self.vocals_mp3 = vlc.MediaPlayer(tempdir + "/" + song_name + "/vocals.mp3")
-    self.drums_mp3 = vlc.MediaPlayer(tempdir + "/" + song_name + "/drums.mp3")
-    self.bass_mp3 = vlc.MediaPlayer(tempdir + "/" + song_name + "/bass.mp3")
-    self.other_mp3 = vlc.MediaPlayer(tempdir + "/" + song_name + "/other.mp3")
+
+    showinfo(
+      title='Loading',
+      message='Loading music, wait for success window.'
+    )
+    generated_path = self.tempdir + "/" + song_name
+    if not (os.path.exists(generated_path)):
+      cut.separate_to_file(self.song_path, self.tempdir, codec='mp3');
+    self.vocals_mp3 = vlc.MediaPlayer(generated_path + "/vocals.mp3")
+    self.drums_mp3 = vlc.MediaPlayer(generated_path + "/drums.mp3")
+    self.bass_mp3 = vlc.MediaPlayer(generated_path + "/bass.mp3")
+    self.other_mp3 = vlc.MediaPlayer(generated_path + "/other.mp3")
     self.song_loaded = True
+    self.pause['state'] = NORMAL
+
+    showinfo(
+      title='Success',
+      message='Music Loaded'
+    )
+
+  def select_song(self):
+    self.song_loaded = False
+    self.pause['state'] = DISABLED
+    self.song_path = fd.askopenfilename(title='Choose a song',initialdir='/', filetypes=[("Mp3 Files", "*.mp3")])
+    print(self.song_path)
+    '''
+    showinfo(
+      title='Selected Song',
+      message=self.song_path
+    )
+    '''
+
+    if ".mp3" in self.song_path:
+      self.setup()
     
 
   def pause_and_play(self):
     if not self.song_loaded:
+      showinfo(
+        title='Error!',
+        message='Load a music first!'
+      )
       return
     if not self.is_playing: 
       self.vocals_mp3.play()
@@ -79,11 +116,12 @@ class App:
       self.bass_mp3.play()
       self.other_mp3.play()
     else:
-      self.vocals_mp3.stop()
-      self.drums_mp3.stop()
-      self.bass_mp3.stop()
-      self.other_mp3.stop()
+      self.vocals_mp3.pause()
+      self.drums_mp3.pause()
+      self.bass_mp3.pause()
+      self.other_mp3.pause()
 
     self.is_playing = not self.is_playing
 
-app=App()
+if __name__ == "__main__":
+  app=App()
