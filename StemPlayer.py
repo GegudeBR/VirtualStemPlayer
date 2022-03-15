32377b2b4ef627629b611a36cd0a9772e25501e0
George <67044516+GegudeBR@users.noreply.github.com>
2022-03-15 15:01:52 -0700

    Added media timeline

--- StemPlayer.py
+++ StemPlayer.py
@@ -19,7 +19,7 @@ class App:
     ttk.Style().theme_use('forest-dark')
 
 
-    # Defining variables
+    # Defining variablesm
     self.is_playing = False
     self.song_loaded = False
     self.vocals_mp3, self.drums_mp3, self.bass_mp3, self.other_mp3 = None, None, None, None
@@ -33,15 +33,14 @@ class App:
     self.drums = ttk.Scale(self.root, from_=100, to=0, orient=HORIZONTAL, command=self.update_drums)
     self.bass = ttk.Scale(self.root, from_=0, to=100, orient=HORIZONTAL, command=self.update_bass)
     self.other = ttk.Scale(self.root, from_=0, to=100, orient=VERTICAL, command=self.update_other)
+    self.media_time = ttk.Scale(self.root, from_=0, to=100, length=280, orient=HORIZONTAL)
     self.song_button = ttk.Button(self.root, text="Select Song",  command=self.select_song)
 
     # Setting sliders to 100%
-    stems = [self.vocals, self.drums, self.bass, self.other]
-    for stem in stems:
-      stem.set(100)
+    self.set_max_stems()
 
     center_x = 140
-    center_y = 135
+    center_y = 130
 
     # Top Mid
     self.vocals.place(x=center_x, y=10)  
@@ -50,10 +49,12 @@ class App:
     #Mid Right
     self.bass.place(x=190, y=center_y)
     #Bottom Mid
-    self.other.place(x=center_x,y=180)
+    self.other.place(x=center_x,y=170)
 
     self.pause.place(x=center_x - 5, y=center_y - 5,height=30, width=30)
     self.song_button.place(x=10, y=10)
+
+    self.media_time.place(x=10, y=280)
 
     # Binds
     self.root.bind('m', self.mute_vocals)
@@ -62,8 +63,25 @@ class App:
     self.root.title("Virtual Stem Player")
     self.root.geometry("300x300")
     self.root.resizable(False, False)
-    self.root.mainloop()
+    while(1):
+      if self.vocals_mp3 != None:
+        if self.media_time['to'] != (self.vocals_mp3.get_length() / 1000):
+          self.media_time['to'] = self.vocals_mp3.get_length() / 1000
+        #print(self.vocals_mp3.get_time() / 1000)
+        self.media_time.set(self.vocals_mp3.get_time() / 1000)
+      self.root.update_idletasks()
+      self.root.update()
 
+  def move_media(self, event):
+    if self.vocals_mp3 != None:
+      stems = [self.vocals_mp3, self.drums_mp3, self.bass_mp3, self.other_mp3]
+      for stem in stems:
+        stem.set_time(int(self.media_time.get() * 1000))
+
+  def set_max_stems(self):
+    stems = [self.vocals, self.drums, self.bass, self.other]
+    for stem in stems:
+      stem.set(100)
 
   def mute_vocals(self, event):
     if int(self.vocals.get()) > 0:
@@ -87,22 +105,25 @@ class App:
       self.other_mp3.audio_set_volume(int(self.other.get())) 
 
   def setup(self):
-  
-    cut = Separator('spleeter:4stems')
-    split_path = self.song_path.split("/")
-    song_name = split_path[len(split_path)-1].replace(".mp3", "")
+    #cut = Separator('spleeter:4stems')
+    #split_path = self.song_path.split("/")
+    #song_name = split_path[len(split_path)-1].replace(".mp3", "")
 
     showinfo(
       title='Loading',
       message='Loading music, wait for success window.'
     )
-    generated_path = self.tempdir + "/" + song_name
-    if not (os.path.exists(generated_path)):
-      cut.separate_to_file(self.song_path, self.tempdir, codec='mp3');
+
+    #generated_path = self.tempdir + "/" + song_name
+    generated_path = "/Users/hallaig/Desktop/Happier_Than_ever"
+    #if not (os.path.exists(generated_path)):
+    #  cut.separate_to_file(self.song_path, self.tempdir, codec='mp3');
     self.vocals_mp3 = vlc.MediaPlayer(generated_path + "/vocals.mp3")
     self.drums_mp3 = vlc.MediaPlayer(generated_path + "/drums.mp3")
     self.bass_mp3 = vlc.MediaPlayer(generated_path + "/bass.mp3")
     self.other_mp3 = vlc.MediaPlayer(generated_path + "/other.mp3")
+
+    self.set_max_stems()
     self.song_loaded = True
     self.pause['state'] = NORMAL
 
